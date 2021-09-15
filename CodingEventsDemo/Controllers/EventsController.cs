39883fcb8b2ff51ccd48c1d17ddc1989a700baeb
samwed78 <84482279+samwed78@users.noreply.github.com>
2021-09-15 15:04:31 -0500

    commit add

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,17 +11,31 @@ namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
+
+        static private List<Event> Events = new List<Event>();
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<string> Events = new List<string>();
-            Events.Add("Code With Pride");
-            Events.Add("Apple WWDC");
-            Events.Add("Strange Loop");
-
             ViewBag.events = Events;
 
             return View();
         }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Events/Add")]
+        public IActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(new Event(name, desc));
+            
+
+            return Redirect("/Events");
+        }
     }
 }
+
---
+++ CodingEventsDemo/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace CodingEventsDemo.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Event(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public Event(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
